091cbdaad0c631521a57ef780db371357ddf3caa
unknown <mario@mustang.ijs.si>
2015-06-21 02:26:09 +0200

    removed duplicate

--- snap-master/snap-core/gcentr.h
+++
@@ -1,4 +0,0 @@
-namespace TSnap {
-  // Group degree centralization
-  void MaxCPGreedy(const PNGraph& Graph, TIntH& GroupNodes, TIntFltVH& ResultVector, TFltV& MaxVector, bool& Unimodal, const int k);
-}; // namespace TSnap
